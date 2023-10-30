45c6e0285b84c62048cfe4c6534aceaaf26668fc
曹珂俭 <1849201815@qq.com>
2023-10-30 14:21:05 +0800

    feat: mobx更改数据

--- src/stores/TodoStore.js
+++ src/stores/TodoStore.js
@@ -1,10 +1,15 @@
-import { observable } from 'mobx';
+import { action, observable } from 'mobx';
 
 class TodoStore {
   @observable
   todos = ['1', '2', '3'];
   @observable
   filter = "";
+
+  @action
+  handleChange = async () => {
+    this.todos = ['已经改了！', 'haha']
+  }
 }
 
 export default new TodoStore();
--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,4 +1,4 @@
-import React, { memo } from 'react'
+import React, { memo, useEffect, useState } from 'react'
 import type { FC, ReactNode } from 'react'
 import { observer } from 'mobx-react';
 import todoStore from '../stores/TodoStore';
@@ -8,10 +8,24 @@ interface IProps {
 
 const Login: FC<IProps> = observer(() => {
   // 你可以在这里使用 todoStore
-  console.log(todoStore.todos);
-
-  return (
+  const [todo, setTodo] = useState(todoStore.todos)
+  const changeTodo = () => {
+    console.log('点击按钮')
+    todoStore.handleChange()
+    setTodo(todoStore.todos)
+  }
+  useEffect(() => {
+    console.log('mobx todo:', todoStore.todos);
+    console.log('local todo:', todo);
+  }, [todo])
+  
+  return (<>
     <div>Login</div>
+    <div>Todo: {todo}</div>
+    <button
+      onClick={changeTodo}
+    >点击改变todos</button>
+  </>
   )
 })
 
